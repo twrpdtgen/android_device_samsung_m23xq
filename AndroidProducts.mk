fb912f0fd0bbbb7291a8117af8ef0cfbf4dd1615
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2022-09-29 00:36:04 +0200

    m23xq: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: samsung Model: SM-M236B Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_m23xq.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_m23xq-user \
+    omni_m23xq-userdebug \
+    omni_m23xq-eng
